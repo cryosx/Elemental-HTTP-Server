a4daf6d210f116541510ed38ac3275dfa5b970b7
cryosx <cyruswu.email@gmail.com>
2018-04-23 13:49:22 -1000

    updated to be more general

--- requestHandler.js
+++ requestHandler.js
@@ -1,30 +1,34 @@
 const fs = require('fs');
 const path = require('path');
+
 const METHODS = { GET: 'GET', PUT: 'PUT', POST: 'POST' };
-const STATUSPHRASE = { OK: 'OK' };
-const OK = 'OK';
+const STATUSPHRASE = { OK: 'OK', NOTFOUND: 'Not Found' };
+
 module.exports = function requestHandler(request, response) {
   const method = request.method.toUpperCase();
   const uri = request.url;
   const filePath = path.join(__dirname, 'public', uri);
+  const errorPath = path.join(__dirname, 'public', '/404.html');
   switch (method) {
     case METHODS.GET:
-      if (uri === '/foo.html') {
-        fs.readFile(filePath, (err, data) => {
-          if (err) throw err;
+      fs.readFile(filePath, (err, data) => {
+        if (err) {
+          // throw err;
+          fs.readFile(errorPath, (err, data) => {
+            response.writeHead(404, STATUSPHRASE.NOTFOUND, {
+              'Content-Type': 'text/html'
+            });
+            return response.end(data);
+          });
+        } else {
           response.writeHead(200, STATUSPHRASE.OK, {
             'Content-Type': 'text/html'
           });
           return response.end(data);
-        });
-      } else {
-        response.writeHead(200, STATUSPHRASE.OK, {
-          'Content-Type': 'text/plain'
-        });
-        return response.end('smoke test');
-      }
-
+        }
+      });
       break;
+
     default:
       return resopnse.end();
       break;
