aad60699b32103630742a66e63b8bee247736439
cryosx <cyruswu.email@gmail.com>
2018-04-23 14:55:48 -1000

    updated reqeustHandler to handle all get requests

--- requestHandler.js
+++ requestHandler.js
@@ -1,28 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
-const METHODS = { GET: 'GET', PUT: 'PUT', POST: 'POST' };
-const STATUSPHRASE = { OK: 'OK', NOTFOUND: 'Not Found' };
+const { GET, PUT, POST } = require('./request_methods');
+const { OK, NOT_FOUND, INTERNAL_SERVER_ERROR } = require('./status_phrases');
 
 module.exports = function requestHandler(request, response) {
   const method = request.method.toUpperCase();
-  const uri = request.url;
+  const uri = request.url === '/' ? '/index.html' : request.url;
   const filePath = path.join(__dirname, 'public', uri);
   const errorPath = path.join(__dirname, 'public', '/404.html');
+
+  let uriSplit = uri.split('?');
+
+  const fileType = uriSplit[0].lastIndexOf('.')
+    ? uri.slice(uri.lastIndexOf('.') + 1)
+    : 'html';
+
   switch (method) {
-    case METHODS.GET:
+    case GET:
       fs.readFile(filePath, (err, data) => {
         if (err) {
-          // throw err;
           fs.readFile(errorPath, (err, data) => {
-            response.writeHead(404, STATUSPHRASE.NOTFOUND, {
-              'Content-Type': 'text/html'
+            if (err) {
+              response.writeHead(500, INTERNAL_SERVER_ERROR);
+              return response.end();
+            }
+            response.writeHead(404, NOT_FOUND, {
+              'Content-Type': `text/html`
             });
             return response.end(data);
           });
         } else {
-          response.writeHead(200, STATUSPHRASE.OK, {
-            'Content-Type': 'text/html'
+          response.writeHead(200, OK, {
+            'Content-Type': `text/${fileType}`
           });
           return response.end(data);
         }
